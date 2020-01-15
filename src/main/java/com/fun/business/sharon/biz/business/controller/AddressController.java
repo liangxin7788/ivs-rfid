3490237297a2aa15d9343b8f22bf8a41c001e5ec
liangxin7788 <lx_4509@163.com>
2020-01-15 11:59:35 +0800

    判空

---
+++ src/main/java/com/fun/business/sharon/biz/business/controller/AddressController.java
@@ -0,0 +1,23 @@
+package com.fun.business.sharon.biz.business.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Package: com.fun.business.sharon.biz.business.controller
+ * @ClassName: AddressController
+ * @Description: java类作用描述
+ * @Author: liangxin
+ * @CreateDate: 2019/12/26 17:46
+ * @UpdateDate: 2019/12/26 17:46
+ */
+@RestController
+@RequestMapping("/address")
+@Api(description = "登录用户地址信息")
+public class AddressController {
+
+
+
+
+}
